49ce324ecf9a190f29f3798994d3ab88378da31d
Luis Felipe <luisfe_617@outlook.com>
2017-08-19 18:11:02 -0500

    old dist version deleted

--- dist/data/chats.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var initDatabase_1 = require("../initDatabase");
-var Chats;
-(function (Chats) {
-    Chats.guardarContextoChat = function (chatId, contexto) {
-        return initDatabase_1.dataBase.ref('chats/' + chatId + '/contexto').set(contexto);
-    };
-})(Chats = exports.Chats || (exports.Chats = {}));
